86663b1871a4fc05d898b1b14fdcd424c52e5a9f
Mike <mihaildaryin@gmail.com>
2021-11-23 21:12:08 +0300

    edit index and main

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-// const hobbit = new book('hobbit', 'tolkin', '200', 'yes');
-
-// console.log(hobbit.info());
-
+let bookTitle = document.getElementById('book_title');
+let bookAuthor = document.getElementById('book_author');
+let bookNumberOfPages = document.getElementById('number_of_pages');
+let bookReadStatus = document.getElementById('read_status');
+let library = document.getElementById('books__list');
+let addNewBook = document.getElementById('add_new_book');
+addNewBook.addEventListener('click', addBookToLibrary);
 let myLibrary = [];
 
 function book (title, author, numberOfPages, readStatus){
-        this.title = title;
-        this.author = author;
-        this.numberOfPages = numberOfPages;
-        this.readStatus = readStatus;
-        this.info = function(){
-            return (title + " by " + author + ',' + numberOfPages + ',' + readStatus);
-        };
-        
+        this.title = bookTitle.value;
+        this.author = bookAuthor.value;
+        this.numberOfPages = bookNumberOfPages.value;
+        this.readStatus = bookReadStatus;
+            return (title, author, numberOfPages, readStatus);
     };
 
 function addBookToLibrary(){
-    return myLibrary.push(book);
+    console.log(this.title)
+    myLibrary.push(book);
+    console.log(myLibrary);
 }
 
-console.log(myLibrary)
+// function addBookToLibrary(){
+//     library.innerHTML = new book('asds', 'auewrethor', 'numbefesfrOfPages', 'readsefStatus');
+//     // return myLibrary.push(book);
+// };
+
