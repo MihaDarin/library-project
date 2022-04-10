ea009eca375f9061e7736169cd1aafd425cfc7c0
Mike <mihaildaryin@gmail.com>
2022-04-10 21:40:59 +0300

    added id to every book in massive

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-let library = document.querySelector('.books__list');
-let addNewBook = document.getElementById('add_new_book');
-addNewBook.addEventListener('click', addBookToLibrary);
-addNewBook.addEventListener('click', showBook);
+let library = document.querySelector(".books__list");
+let addNewBook = document.getElementById("add_new_book");
+addNewBook.addEventListener("click", addBookToLibrary);
+addNewBook.addEventListener("click", showBook);
 let myLibrary = [];
 
-class book{
-    constructor(title, author, numberOfPages, readStatus){
-        this.title = document.getElementById('book_title').value;
-                this.author = document.getElementById('book_author').value;
-                this.numberOfPages = document.getElementById('number_of_pages').value;
-                this.readStatus = document.getElementById('read_status').value;
-                return;
-    }
-}
-function addBookToLibrary(){
-    let newBook = new book();
-    myLibrary.push(newBook);
-    console.log(myLibrary)
-};
+class book {
+  constructor(title, author, numberOfPages, readStatus) {
+    this.title = document.getElementById("book_title").value;
+    this.author = document.getElementById("book_author").value;
+    this.numberOfPages = document.getElementById("number_of_pages").value;
+    this.readStatus = document.getElementById("read_status").value;
+    this.id = 0;
+  }
 
-function showBook(){
-    let form;
-        for(let i = 0; i < myLibrary.length; i++){ 
-            form =
-            `<form class="book__form">
+}
+
+function addBookToLibrary() {
+  let newBook = new book();
+  newBook.id = myLibrary.length;
+  myLibrary.push(newBook);
+  // console.log(myLibrary);
+  // console.log(newBook.id);
+}
+
+function showBook() {
+  let form;
+  for (let i = 0; i < myLibrary.length; i++) {
+    form = `<form class="book__form">
                 <fieldset class="book_info">
                     <label for="book_title">
                         Book Title
@@ -40,7 +43,12 @@ function showBook(){
                     <label for="read_status">
                         Read Status
                         <input type="checkbox" name="read_status" id="read_status">
-                    </label>`
-    };
-    library.innerHTML += form;
-};
+                    </label>
+                    <label for="delete_book">
+                    <input type="button" name="delete_book" id="delete_book" value="Delete Book">
+                </label>
+                   `;
+  }
+  
+  library.innerHTML += form;
+}
